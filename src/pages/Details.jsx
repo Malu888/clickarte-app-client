dbd8f330f01e583adacc9825d4005d357b890d11
unknown <maludietrich.00@gmail.com>
2024-09-28 19:41:12 +0200

    second commit

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-function Details() {
+
+
+function Details({ allData, setAllData, comment, setComment }) {
+  
+const {detailsId} = useParams()
+
+console.log(detailsId)
+
+const imgInformation = allData.find((eachElement) => eachElement.imagenid === detailsId)
+const commentInformation = comment.find((eachElement) => eachElement.imagenid === detailsId)
+
+
+  
   return (
-    <div>Details</div>
+    <div>
+      <p>Details Page</p>
+      <img src={imgInformation.img} alt={`imagen ${imgInformation.imagenid}`} style={{ width: '100px' }} />
+      <p>Categoria:{imgInformation.categoria}</p>
+      <p>Location: {imgInformation.location}</p>
+      <p>Autor: {commentInformation.autor}</p>
+      <p>Rating: {commentInformation.rating}</p>
+      <p>Contenido: {commentInformation.contenido}</p>
+
+
+      
+      
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,10 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
-function Home() {
-    const [allData, setAllData] = useState(null)
 
-    useEffect(() => {
-        getData()
-    }, [])
+function Home({allData, setAllData}) {
 
-    const getData = async () => {
-
-        try {
-            const response = await axios.get(`http://localhost:5000/imagenes`);
-            console.log(response)
-            setAllData(response.data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    if (allData === null) {
-        return <p>Loading...</p>
-    }
 
     return (
         <>
@@ -33,7 +14,9 @@ function Home() {
                         return (
                             <div key={i}>
                                 <p>Hola</p>
-                                <img src={eachElement.img} alt={`imagen ${i}`} style={{width: '50px'}}/>
+                                <Link to={`details/${eachElement.imagenid}`}>
+                                <img src={eachElement.img} alt={`imagen ${i}`} style={{ width: '50px' }} />
+                                </Link>
                             </div>
                         )
                     })}
